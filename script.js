d3f65c32cf4ddf19b98509b31e54ff66641e3609
KifilweJasmin <kifilwejm@gmail.com>
2025-05-07 00:29:41 +0200

    Improved the user experience

--- script.js
+++ script.js
@@ -21,6 +21,10 @@ function generatePoem(event) {
     let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML, dont write 'HTML' and separate each line with a <br/>. Make sure to follow the users instructions. Sign the poem with 'SheCodes AI'inside a <strong> element at the end of the poem.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating"> ⌛️Generating a poem about a ${instructionsInput.value}</div>`
+
     console.log("Generating poem");
     console.log(`Prompt: ${prompt}`);
     console.log(`Context: ${context}`);
