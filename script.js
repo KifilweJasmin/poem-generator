96d0e5a7a9c4303f9804e9a3f8867b05933585b6
KifilweJasmin <kifilwejm@gmail.com>
2025-05-07 00:33:19 +0200

    Removed the console log

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function displayPoem(response) {
 
-    console.log("poem generated")
 new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -25,9 +24,7 @@ function generatePoem(event) {
     poemElement.classList.remove("hidden");
     poemElement.innerHTML = `<div class="generating"> ⌛️Generating a poem about a ${instructionsInput.value}</div>`
 
-    console.log("Generating poem");
-    console.log(`Prompt: ${prompt}`);
-    console.log(`Context: ${context}`);
+    
     axios.get(apiURL).then(displayPoem);
 }
 
